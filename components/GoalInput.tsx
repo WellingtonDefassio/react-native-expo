86a87a31fba4fda014db98779acdd6add9c0e2d5
WellingtonDefassio <well.defassio@gmail.com>
2023-12-10 14:30:15 -0300

    add style for modal

--- components/GoalInput.tsx
+++ components/GoalInput.tsx
@@ -25,7 +25,14 @@ export default function GoalInput(props: GoalInputProps) {
                     onChangeText={setGoalTextState}
                     value={goalTextState}
                 />
-                <Button title={"Add Goal"} onPress={addGoalText}/>
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title={"Add Goal"} onPress={addGoalText}/>
+                    </View>
+                    <View style={styles.button}>
+                        <Button title={"Cancel"}/>
+                    </View>
+                </View>
             </View>
         </Modal>
     );
@@ -35,18 +42,25 @@ export default function GoalInput(props: GoalInputProps) {
 const styles = StyleSheet.create({
     inputContainer: {
         flex: 1,
-        flexDirection: "row",
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         alignItems: "center",
         marginBottom: 24,
+        padding: 16,
         borderBottomWidth: 1,
         borderBottomColor: "#ccc",
     },
     textInput: {
         borderWidth: 1,
         borderColor: "#ccc",
-        width: "70%",
-        marginRight: 8,
+        width: "100%",
         padding: 8
     },
+    buttonContainer: {
+        marginTop: 16,
+        flexDirection: "row"
+    },
+    button: {
+        width: 100,
+        marginHorizontal: 8
+    }
 })
